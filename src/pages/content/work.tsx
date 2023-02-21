1c0f4cdf4e894ce397017fbcea9688eaafe821f8
Keenan Aldridge <kmantan@gmail.com>
2023-02-20 19:25:07 -1000

    Broke work page into three sections with project names

--- src/pages/content/work.tsx
+++ src/pages/content/work.tsx
@@ -2,18 +2,17 @@ import { useState } from "react";
 
 export default function Work(){
   return (
-    <div className="m-10">
-      <h1 className="text-4xl text-center m-10">Here Are My Projects</h1>
-      <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam enim justo, finibus vitae neque quis, varius fermentum lorem. Curabitur varius nisi dolor, ac ultricies lacus rhoncus vel. Proin varius quam nec elit finibus accumsan. Donec in ante placerat, fringilla mi et, euismod lectus. In at enim nec elit fermentum pretium. Pellentesque et elit semper, volutpat felis quis, facilisis quam. Duis ac auctor magna, sed egestas dui. Donec est justo, vulputate at ornare eget, tristique ut massa. Etiam rhoncus arcu ut augue tincidunt convallis. Proin consectetur ultricies turpis pretium venenatis. Sed eget elit quis justo interdum iaculis non eu sem.
-
-Aenean sollicitudin mi metus. Etiam eu lacus nisi. Duis feugiat efficitur sem id tempus. Ut et sem id velit mattis laoreet ultricies sit amet lacus. Pellentesque dolor lorem, porttitor in finibus et, varius in orci. Fusce in erat in nunc facilisis pellentesque. Vivamus vitae velit quis orci aliquam tincidunt vel venenatis ex. Pellentesque sit amet hendrerit eros.
-
-Mauris sed posuere lectus, a auctor leo. Praesent eget bibendum turpis, vitae congue turpis. Maecenas pellentesque, mi id ullamcorper feugiat, dolor lorem sodales lectus, id hendrerit turpis sapien a nunc. Morbi sed lectus nulla. Nullam mauris tellus, sodales eu diam vel, tincidunt tincidunt leo. Praesent convallis eu magna in ornare. Integer at purus quis justo cursus eleifend sit amet a leo. Phasellus eleifend vehicula neque, a egestas velit sagittis id. In in augue vel arcu efficitur convallis sed nec massa. Aliquam tincidunt sollicitudin venenatis. Nunc vitae nisi eget arcu blandit bibendum. Proin sed turpis dui. Aliquam dictum lacus ac felis consectetur finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed mattis ac diam vitae bibendum. Cras mattis orci at nibh feugiat, sit amet placerat leo lobortis.
-
-Curabitur et fringilla neque. Aliquam diam ligula, vestibulum vel aliquet lobortis, fringilla vitae lorem. Proin lacinia libero eu nunc tempus, ac ultrices nisl sagittis. Phasellus gravida eros arcu, sodales fringilla erat laoreet id. Donec placerat, dui nec cursus dictum, nisi orci facilisis enim, laoreet pretium nunc purus id nunc. Nunc malesuada neque vitae nunc pellentesque, ac malesuada mi rutrum. Maecenas tempor, tortor at commodo dictum, dui magna sollicitudin odio, non mattis eros arcu a nibh. Praesent at ultricies dui. Nam imperdiet elementum neque, interdum lobortis nisl ullamcorper vel. Integer placerat dui non fermentum sollicitudin. Aenean eu lorem nisi. Sed pulvinar blandit massa id pretium.
-
-Vivamus vel magna at libero accumsan pretium quis et lorem. In arcu nunc, facilisis in enim sit amet, consequat facilisis odio. Sed pretium, elit eleifend luctus rutrum, augue leo hendrerit lectus, eget blandit risus enim eu lorem. Vivamus dignissim leo et molestie pretium. Aenean ullamcorper non ex id semper. Nullam laoreet consectetur magna, ac placerat ex. Cras accumsan elit felis, ac pellentesque diam viverra nec. Morbi scelerisque tellus quis vestibulum sollicitudin. Nullam condimentum aliquet mauris sit amet faucibus. Phasellus malesuada sem eu convallis vestibulum. Maecenas gravida quam eu congue iaculis.</p>
+    <div className="work">
+      <section className="poke">
+        <h2>Pokedex</h2>
+        <p>Here is the Pokedex app I made</p>
+      </section>
+      <section className="atelier">
+        <h2>Atelier</h2>
+      </section>
+      <section className="health">
+        <h2>My Health Coach</h2>
+      </section>
     </div>
   )
 };
