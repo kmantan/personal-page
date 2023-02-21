339f0ce075a9ae9d1003f0b310ad39fed802ff99
Keenan Aldridge <kmantan@gmail.com>
2023-02-20 16:50:16 -1000

    Fixed render issue and config file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" style={{
-      height: "100%",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center"
+      height: "100%"
     }}>
       <Head />
       <body>
