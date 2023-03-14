815c030256c2bffb3ae0bccb2a3ef08c82c50473
Keenan Aldridge <kmantan@gmail.com>
2023-03-13 16:16:28 -1000

    Added video for My Health Coach with placeholder video, needs actual video.

--- src/pages/apps/my-health-coach/HealthCoach.tsx
+++ src/pages/apps/my-health-coach/HealthCoach.tsx
@@ -3,11 +3,16 @@ import ScrollForMore from '../../../commonFunctionalities/ScrollForMore.jsx'
 
 export default function HealthCoach() {
   return (
-    <div className="relative h-full flex flex-col items-center">
-      <h2>My Health Coach</h2>
-      <div className="absolute bottom-20">
-      <ScrollForMore />
+    <>
+      <h2 className="mb-10">My Health Coach</h2>
+      <div className="relative w-full flex flex-col items-center justify-center overflow-hidden h-full">
+        <div className="container">
+          <iframe className="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+        </div>
+        <div className="absolute bottom-20">
+          <ScrollForMore />
+        </div>
       </div>
-    </div>
+    </>
   )
 };
