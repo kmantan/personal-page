2c34701aac01cd28d07138570356255592b2c532
Keenan Aldridge <kmantan@gmail.com>
2023-03-14 21:05:23 -1000

    Updated links for videos in work

--- src/pages/apps/atelier/Atelier.tsx
+++ src/pages/apps/atelier/Atelier.tsx
@@ -7,7 +7,7 @@ export default function Atelier() {
       <h2 className="mb-10">Atelier</h2>
       <div className="relative w-full flex flex-col items-center justify-center overflow-hidden h-full">
         <div className="container">
-          <iframe className="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+          <iframe className="responsive-iframe" src="https://www.youtube.com/embed/7eDj7nY_trY" allowFullScreen={true}></iframe>
         </div>
       </div>
     </>
--- src/pages/apps/my-health-coach/HealthCoach.tsx
+++ src/pages/apps/my-health-coach/HealthCoach.tsx
@@ -7,7 +7,7 @@ export default function HealthCoach() {
       <h2 className="mb-10">My Health Coach</h2>
       <div className="relative w-full flex flex-col items-center justify-center overflow-hidden h-full">
         <div className="container">
-          <iframe className="responsive-iframe" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+          <iframe className="responsive-iframe" src="https://www.youtube.com/embed/otZ0EkmshAQ" allowFullScreen={true}></iframe>
         </div>
         <div className="absolute bottom-20">
           <ScrollForMore />
